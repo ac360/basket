4d11869645478172624a30d68cf10d166862b0c3
Austen Collins <austencollins@gmail.com>
2013-12-10 23:25:23 -0800

    Animated button

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,11 +1,13 @@
 
 
 mZoomIn = function(e){
-var moveBack = move(e).scale(1)
+var settleBack = move(e)
+  .scale(1)
+  .x(0)
 move(e)
   .set('opacity', 1)
   .scale(1.1)
-  .then(moveBack)
-  .duration('0.4s')
+  .then(settleBack)
+  .duration('0.2s')
   .end();
 };
