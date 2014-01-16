15549f05cd4cce05ab02ff8075a3ca56f961d4be
Austen Collins <austencollins@gmail.com>
2014-01-16 10:53:49 -0800

    changes

--- public/js/controllers/root.js
+++ public/js/controllers/root.js
@@ -260,7 +260,7 @@ angular.module('mean.system').controller('RootController', ['$scope', 'Global', 
         window.fbAsyncInit = function() {
             // init the FB JS SDK
             FB.init({
-              appId      : '252087231617494',                    // Dev: 252087231617494 Pro: 736751053015158
+              appId      : '736751053015158',                    // Dev: 252087231617494 Pro: 736751053015158
               status     : true,                                 // Check Facebook Login status
               xfbml      : true                                  // Look for social plugins on the page
             });
