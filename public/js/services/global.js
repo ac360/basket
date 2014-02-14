56de59ec452ac6f3486bfe34f418d7238a923fdd
Austen Collins <austencollins@gmail.com>
2014-02-13 23:35:00 -0800

    changes

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -1,6 +1,6 @@
 angular.module('mean.system').factory("Global", ['$http', '$rootScope', '$modal', '$state', '$stateParams', "Users", "Modals", "Medleys", "Votes", "Folders", function($http, $rootScope, $modal, $state, $stateParams, Users, Modals, Medleys, Votes, Folders) {
     
-	var mData = { user: null, folders: null, medley: { items: [] } };
+	var mData = { user: null, folders: null, medley: { items: [], template: 'a1' } };
 
     return {
     		// ---------- MEDLEYS ----------
