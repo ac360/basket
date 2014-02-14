bbb6e6270fda98d551ce9965591f642f2e038bfb
Austen Collins <austencollins@gmail.com>
2014-02-13 22:49:24 -0800

    changes

--- public/js/init.js
+++ public/js/init.js
@@ -7,6 +7,15 @@ window.init = function() {
     window.bootstrap();
 };
 
+// Set Facebook Key
+var facebookKey = false;
+var host = window.location.hostname;
+if (~host.indexOf('mdly')) {
+	facebookKey = '736751053015158';
+} else {
+	facebookKey = '252087231617494';
+};
+
 $(document).ready(function() {
     //Fixing facebook bug with redirect
     if (window.location.hash && window.location.hash == '#_=_') {
